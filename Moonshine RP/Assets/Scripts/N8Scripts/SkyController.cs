be7795e3a770b4abc4306b2c0c104c60f4db9733
N8 <ndavis24@mail.csuchico.edu>
2024-02-13 13:50:52 -0800

    SkyBox

---
+++ Moonshine RP/Assets/Scripts/N8Scripts/SkyController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyController : MonoBehaviour
+{
+    private Material SkyBox;
+    public float Speed;
+    private float tFactor;
+    public bool isNight;
+
+    public float DayValue;
+    public float NightValue;
+    public float changeSpeed;
+    private void Start()
+    {
+        SkyBox = RenderSettings.skybox;
+    }
+
+
+    private void Update()
+    {
+        SkyBox.SetFloat("_Rotation", SkyBox.GetFloat("_Rotation") + Time.deltaTime * Speed);
+
+
+        if (isNight == true && tFactor >= NightValue)
+        {
+            tFactor -= Time.deltaTime * changeSpeed;
+
+            SkyBox.SetColor("_Tint",new Color(tFactor, tFactor, tFactor, 1));
+
+        }
+
+          else if (isNight == false && tFactor <= DayValue)
+        {
+            tFactor += Time.deltaTime * changeSpeed;
+
+            SkyBox.SetColor("_Tint",new Color(tFactor, tFactor, tFactor, 1));
+
+        }
+
+    }
+
+}
